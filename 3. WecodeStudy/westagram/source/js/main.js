caa6ba3d007ba12a0c21eb8380914b8e64c59997
PARKJONGSUN88 <parksunl@users.noreply.github.com>
2020-05-05 18:03:23 +0900

    이벤트코드 정리

--- 3. WecodeStudy/westagram/source/js/main.js
+++ 3. WecodeStudy/westagram/source/js/main.js
@@ -1,26 +1,38 @@
-addEventListener('keyup', function() {
-    let comment = document.getElementsByClassName("comment-write")[0].value;
-    if (comment.length !== 0) {
-        document.getElementsByClassName("submitButton")[0].classList.add("clickbutton");
-    }
-});
+/* 댓글 게시 버튼 요소겟 */
+const submitButtonClick = document.getElementsByClassName("submitButton")[0];
+const commentDone = document.getElementsByClassName("comment-write")[0];
 
-let superUser = "js_Park"
-function commentWrite() {
-    let result = document.getElementsByClassName("comment-view-div")[0];
+/* 댓글쓰는 곳에 내용이 null이 아니면 css가 변경됨 */
+commentDone.addEventListener("keydown", function(e) {
+    if (commentDone.value.length !== null) {
+        submitButtonClick.classList.add("clickbutton") //clickbutton이 변경될 css 클래스
+    }
+})
+
+/* 댓글쓴사람명 임시값 */
+const superUser = "js_Park"
+
+/* 댓글달기 기능 함수 */
+function commentWrite() { 
     let btag = document.createElement("b");
-    btag.innerHTML = superUser;
-    let commentText = document.getElementsByClassName("comment-write")[0].value;
-    let commentViewSpan = document.createElement('span');    
+    btag.innerHTML = superUser;    
+    let commentViewSpan = document.createElement('span'); 
+    let commentText = commentDone.value;       
     commentViewSpan.innerHTML = commentText;
-    let brtag = document.createElement("br");
-    result.appendChild(btag);
+    let result = document.getElementsByClassName("comment-view-div")[0];
+    result.appendChild(btag);    
     result.appendChild(commentViewSpan);
+    let brtag = document.createElement("br");
     result.appendChild(brtag);  
 }
 
-let enterPass = document.getElementsByClassName("comment-write")[0];
-enterPass.addEventListener('keydown', function(e) {
+/* 게시 버튼 클릭 시 댓글함수 실행 */
+submitButtonClick.addEventListener("click", function() {
+    commentWrite();
+});
+
+/* 엔터키 눌렸을 때 댓글함수 실행 */
+commentDone.addEventListener('keydown', function(e) {    
     if (e.keyCode === 13 ) {
         commentWrite();
     }
